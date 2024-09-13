54de11a9d5e8d54a71f6ff275aa497f46074afde
CTC97 <pixelq6@gmail.com>
2024-09-13 00:16:28 -0400

    functional :beers:

--- gnuddha.rb
+++ gnuddha.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gnuddha < Formula
   desc "Meditation UI in your favorite bash terminal. Homebrew distribution coming soon! "
   homepage ""
@@ -13,26 +10,12 @@ class Gnuddha < Formula
   depends_on "mpg123"
   depends_on "curl"
 
-  # depends_on "cmake" => :build
-
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     bin.install "gnuddha.sh" => "gnuddha"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     (share/"sprites").install Dir["sprites/*"]
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gnuddha`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "false"
   end
 end
