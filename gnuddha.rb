648470139f29a27b06958ece8554b066594496fd
CTC97 <pixelq6@gmail.com>
2024-09-15 15:18:53 -0400

    0.1.h

--- gnuddha.rb
+++ gnuddha.rb
@@ -1,8 +1,8 @@
 class Gnuddha < Formula
   desc "Meditation UI in your favorite bash terminal. Homebrew distribution coming soon! "
   homepage ""
-  url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.g.tar.gz"
-  sha256 "da1687092649026c95df1339de94f013edfe427d30ca462a82aa4688db840ebf"
+  url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
+  sha256 "7495482f8663a013ac42611a3af72409ff0876a6204e4b538d0565f3ea16d190"
   license ""
 
   depends_on "jq"
@@ -12,9 +12,7 @@ class Gnuddha < Formula
 
   def install
     bin.install "gnuddha.sh" => "gnuddha"
-    (share/"gnuddha"/"sprites").install Dir["sprites/*"]
-    (share/"gnuddha"/"docs").install "dhamma.json"
-    (share/"gnuddha"/"audio").install "bells-1-72261.mp3"
+    (share/"gnuddha"/"res").install Dir["res/*"]
   end
 
   test do
