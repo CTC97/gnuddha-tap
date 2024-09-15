208e535c8d3943bbd386f87ff0d9e7031b59cb13
CTC97 <pixelq6@gmail.com>
2024-09-15 15:31:01 -0400

    license

--- gnuddha.rb
+++ gnuddha.rb
@@ -3,7 +3,7 @@ class Gnuddha < Formula
   homepage "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
   url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
   sha256 "7495482f8663a013ac42611a3af72409ff0876a6204e4b538d0565f3ea16d190"
-  license ""
+  license "BSD-3-Clause"
 
   depends_on "bc"
   depends_on "jq"
