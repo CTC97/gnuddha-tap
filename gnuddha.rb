f7c8c90215d581692f95338a089a22317268918f
CTC97 <pixelq6@gmail.com>
2024-09-15 15:23:51 -0400

    brew audit fixes

--- gnuddha.rb
+++ gnuddha.rb
@@ -1,14 +1,14 @@
 class Gnuddha < Formula
-  desc "Meditation UI in your favorite bash terminal. Homebrew distribution coming soon! "
-  homepage ""
+  desc "Meditation for healthy, productive deving."
+  homepage "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
   url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
   sha256 "7495482f8663a013ac42611a3af72409ff0876a6204e4b538d0565f3ea16d190"
   license ""
 
-  depends_on "jq"
   depends_on "bc"
-  depends_on "mpg123"
+  depends_on "jq"
   depends_on "curl"
+  depends_on "mpg123"
 
   def install
     bin.install "gnuddha.sh" => "gnuddha"
