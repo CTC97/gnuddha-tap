8d3282e88d3f2da1bb7fb0259f12e5bccefe0efc
CTC97 <pixelq6@gmail.com>
2024-09-15 16:10:36 -0400

    0.1.i

--- gnuddha.rb
+++ gnuddha.rb
@@ -1,8 +1,8 @@
 class Gnuddha < Formula
   desc "Meditation for healthy, productive deving"
   homepage "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
-  url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.h.tar.gz"
-  sha256 "7495482f8663a013ac42611a3af72409ff0876a6204e4b538d0565f3ea16d190"
+  url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.i.tar.gz"
+  sha256 "7b31b1cdc819eb1231545e1bb594cccd8dedc122a01cdc9f1c9b7d2454abe661"
   license "BSD-3-Clause"
 
   depends_on "bc"
