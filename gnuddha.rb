75cf5f4dc77517cd04cbedeeb3924de84f91938a
CTC97 <pixelq6@gmail.com>
2024-09-22 18:04:29 -0400

    0.1.q - pkgshare - brew audit --strict fix

--- gnuddha.rb
+++ gnuddha.rb
@@ -1,8 +1,8 @@
 class Gnuddha < Formula
   desc "Meditation for healthy, productive devving"
   homepage "https://github.com/CTC97/gnuddha"
-  url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.m.tar.gz"
-  sha256 "860a1a61353a8681e8134ae6093617e08afaf3f31f8839f657f41fad4d037c31"
+  url "https://github.com/CTC97/gnuddha/archive/refs/tags/0.1.q.tar.gz"
+  sha256 "c80c77bfc3a031bd35b8f148c57f800e40caf43f6eeab20578e3189b12a98352"
   license "BSD-3-Clause"
 
   depends_on "bc"
@@ -12,12 +12,12 @@ class Gnuddha < Formula
 
   def install
     bin.install "gnuddha.sh" => "gnuddha"
-    (share/"gnuddha"/"res").install Dir["res/*"]
+    pkgshare.install Dir["res/*"]
   end
 
   test do
     assert_predicate bin/"gnuddha", :exist?, "gnuddha executable must exist"
-    assert_predicate share/"gnuddha"/"res", :exist?, "Resource directory must exist"
+    assert_predicate pkgshare, :exist?, "Resource directory must exist"
     puts "Success."
   end
 end
